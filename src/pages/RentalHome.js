db37072e7130a9177bb873e949ccaa2d1712be6a
dbstj0403 <wcn09088@naver.com>
2024-01-30 17:51:05 +0900

    기기대여 페이지 width 수정

--- src/pages/RentalHome.js
+++ src/pages/RentalHome.js
@@ -1,37 +1,35 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
-import { useState } from 'react'
-import point from '../img/point.png';
-import Smartphone from '../components/Smartphone.js'
-import Tablet from '../components/Tablet.js'
-import Notebook from '../components/Notebook.js'
-
+import { useState } from "react";
+import point from "../img/point.png";
+import Smartphone from "../components/Smartphone.js";
+import Tablet from "../components/Tablet.js";
+import Notebook from "../components/Notebook.js";
 
 export default function RentalHome() {
-
   // category 창 (smartphone, tablet, notebook)
   const [category, setCategory] = useState("smartphone");
 
-  // category 선택 버튼 기능 
-  const handleClickButton = e => {
+  // category 선택 버튼 기능
+  const handleClickButton = (e) => {
     const { name } = e.target;
     setCategory(name);
-  }
+  };
 
-  // 카테고리 창에 해당하는 컴포넌트 
+  // 카테고리 창에 해당하는 컴포넌트
   const selectCategory = {
     smartphone: <Smartphone />,
     tablet: <Tablet />,
-    notebook: <Notebook />
-  }
+    notebook: <Notebook />,
+  };
 
-  // 스타일 컴포넌트 
+  // 스타일 컴포넌트
 
   const RentalHomeContainer = styled.div`
-    width: 1440px;
+    width: 100%;
     margin: auto;
-    padding: 140px 110px 210px 110px;
-  `
+    padding: 0px 110px 210px 110px;
+  `;
   const RentalTitle = styled.div`
     margin: auto;
     color: var(--gray-4, #717171);
@@ -39,15 +37,15 @@ export default function RentalHome() {
     font-size: 16px;
     font-weight: 400;
     line-height: 30px;
-    padding: 34px 384px 20px 384px;
+    padding: 34px 350px 20px 350px;
     span {
-      color: var(--gray-5, #2C2C2C);
+      color: var(--gray-5, #2c2c2c);
       text-align: center;
       font-size: 40px;
       font-weight: 700;
       line-height: 66px;
     }
-  `
+  `;
   const RentalCategory = styled.div`
     margin: auto;
     display: flex;
@@ -55,13 +53,13 @@ export default function RentalHome() {
     gap: 30px;
     justify-content: center;
     padding: 20px 20px 40px 20px;
-  `
+  `;
   const CategoryBtn = styled.button`
     width: 170px;
     height: 60px;
     border-radius: 50px;
-    border: 1px solid var(--gray-2, #E3E3E3);
-    background: #FFF;
+    border: 1px solid var(--gray-2, #e3e3e3);
+    background: #fff;
     box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.15);
     color: #000;
     text-align: center;
@@ -70,7 +68,7 @@ export default function RentalHome() {
     font-weight: 700;
     line-height: 24px;
     cursor: pointer;
-  `
+  `;
   const RentalParagraph = styled.div`
     margin: auto;
     padding: 20px 40px 20px 40px;
@@ -78,8 +76,8 @@ export default function RentalHome() {
     justify-content: center;
     width: 840px;
     border-radius: 30px;
-    background: var(--yellow-1, #F9F1DF);
-  `
+    background: var(--yellow-1, #f9f1df);
+  `;
   const ParagraphText = styled.div`
     margin: 0px 20px 0px;
     justify-content: center;
@@ -89,36 +87,44 @@ export default function RentalHome() {
     font-weight: 400;
     line-height: 30px;
     letter-spacing: -0.9px;
-  `
+  `;
   const RentalDevice = styled.div`
     margin: auto;
-    display: flex; 
+    display: flex;
     padding: 50px;
-  `
+  `;
 
   return (
     <RentalHomeContainer>
       <RentalTitle>
-        <span>전자기기 대여</span><br />
-        프로그램을 통해 얻은 포인트로 전자기기를 대여하세요!
+        <span>전자기기 대여</span>
+        <br />
+        <div>프로그램을 통해 얻은 포인트로 전자기기를 대여하세요!</div>
       </RentalTitle>
       <RentalCategory>
-        <CategoryBtn onClick={handleClickButton} name={"smartphone"}>📱 스마트폰</CategoryBtn>
-        <CategoryBtn onClick={handleClickButton} name={"tablet"}>📕 태블릿</CategoryBtn>
-        <CategoryBtn onClick={handleClickButton} name={"notebook"}>💻 노트북</CategoryBtn>
-
+        <CategoryBtn onClick={handleClickButton} name={"smartphone"}>
+          📱 스마트폰
+        </CategoryBtn>
+        <CategoryBtn onClick={handleClickButton} name={"tablet"}>
+          📕 태블릿
+        </CategoryBtn>
+        <CategoryBtn onClick={handleClickButton} name={"notebook"}>
+          💻 노트북
+        </CategoryBtn>
       </RentalCategory>
       <RentalParagraph>
-        <img src={point} alt='' style={{ width: '30px', height: '30px' }} />
+        <img src={point} alt="" style={{ width: "30px", height: "30px" }} />
         <ParagraphText>
-          각 전자기기에 할당된 포인트는 대여 및 연장 시 학생이 보유해야 하는 최소 포인트입니다.<br />
-          꾸준한 프로그램 활동을 통해 포인트를 유지해 대여 기간을 연장할 수 있습니다.
+          각 전자기기에 할당된 포인트는 대여 및 연장 시 학생이 보유해야 하는
+          최소 포인트입니다.
+          <br />
+          꾸준한 프로그램 활동을 통해 포인트를 유지해 대여 기간을 연장할 수
+          있습니다.
         </ParagraphText>
       </RentalParagraph>
       <RentalDevice>
         {category && <div>{selectCategory[category]}</div>}
       </RentalDevice>
-
     </RentalHomeContainer>
-  )
+  );
 }
